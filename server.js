ee82b7908d8a3b630ec4d1c0e94185cfe9a326ab
BhaumikKhandela <152811705+BhaumikKhandela@users.noreply.github.com>
2025-10-31 09:56:18 +0530

    socket

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
@@ -8,6 +13,31 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-server.listen(5000, () => {
-    console.log('Server is running on port 3000');
-}) 
+const io = new Server(server, {
+    cors: {
+        origin: process.env.ELECTRON_HOST,
+        methods: ['GET', 'POST']
+
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('socket is connected')
+    socket.on('video-chunks', async (data) => {
+        console.log('video-chunks received at server', data);
+    });
+
+    socket.on('process-video', async (data) => {
+        console.log('processing video', data);
+    });
+
+    socket.on('disconnect', async(data) => {
+        console.log('socket.id is disconnected', socket.id);
+    })
+})
+
+server.listen(5001, () => {
+    console.log('Server is running on port 5001');
+});
+
+ 
